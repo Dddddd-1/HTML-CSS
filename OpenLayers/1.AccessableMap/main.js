0007d96361ccbc18048d0157f5852969e6c3354e
rhz <renhz>
2022-08-01 15:15:00 +0800

    1

--- OpenLayers/1.AccessableMap/main.js
+++ OpenLayers/1.AccessableMap/main.js
@@ -2,15 +2,25 @@ import Map from 'ol/Map';
 import OSM from 'ol/source/OSM';
 import TileLayer from 'ol/layer/Tile';
 import View from 'ol/View';
+import * as ol from "ol";
 
+new ol.Map({
+
+})
+//创建地图
 const map = new Map({
+    //设置地图图层
     layers: [
+        // 创建一个使用Open Street Map地图源的瓦片图层
         new TileLayer({
             source: new OSM(),
         }),
     ],
+    // 让id为map的div作为地图的容器
     target: 'map',
+    // 设置显示地图的视图
     view: new View({
+        //视图初始中心
         center: [0, 0],
         zoom: 2,
     }),
